eb559517dd77b30643cbb9f00178b0d0d7c38013
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-12-10 03:39:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+function getFirstSelector(selector) {
+  let element = document.querySelectorAll(selector)
+  return element[0]
+}
+
+function nestedTarget() {
+  let element = document.getElementById('nested').getElementsByClassName('target')
+  return element[0]
+}
+
+function deepestChild() {
+  let lastChild = document.getElementById('grand-node').querySelectorAll('div')
+  return lastChild[lastChild.length - 1]
+}
+
+function
